6274012333acc2044d69491f5db13237e24d0b97
dccuong <cuongksv10@mail.com>
2023-01-09 14:55:09 +0700

    ssss

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>heiiiii</Text>
+      <Text>heiiiii</Text>
+      <Text>heiiiii</Text>
+      <Text> heiiiii</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
